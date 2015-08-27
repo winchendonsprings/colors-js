0034634cfaac169a9c3cb550169dd55af249c782
Ryan Greene <winchendonsprings@gmail.com>
2015-08-27 13:15:04 -0700

    neat added

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,16 +3,20 @@ var sass = require('gulp-sass');
 
 //Takes the scss files and converts them to css and will print an error if needed
 gulp.task('styles', function() {
-    gulp.src('scss/*.scss')
+    gulp.src('scss/**/*.scss')
         // .pipe(sass().on('error', sass.logError))
         .pipe(sass({
-            includePaths: require('node-bourbon').includePaths
+            includePaths: require('node-bourbon').includePaths,
+            includePaths: require('node-neat').includePaths
         }))
+//	.pipe(sass({
+//            includePaths: require('node-neat').includePaths
+//        }))
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('css/'));
 });
 
 //Watch task
 gulp.task('default', function() {
-    gulp.watch('scss/*.scss',['styles']);
+    gulp.watch('scss/**/*.scss',['styles']);
 });
