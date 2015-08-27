975968d9f29c7b1c37887cb6c0e87849917ff854
Ryan Greene <winchendonsprings@gmail.com>
2015-08-27 13:16:42 -0700

    clean up script

--- js/script.js
+++ js/script.js
@@ -1,22 +1,18 @@
-function changeClass() {
-}
-
 function changeRed() {
-    document.getElementById("#area").className = ".make-red";
+  document.getElementById("area").className = "make-red";
 }
 
 function changeGreen() {
-    document.getElementById("#area").className = ".make-green";
+  document.getElementById("area").className = "make-green";
 }
 
 function changeBlue() {
-    document.getElementById("#area").className = ".make-blue";
+  document.getElementById("area").className = "make-blue";
 }
 
 
-
 window.onload = function() {
-    document.getElementById("#red").addEventListener( 'click' , changeRed );
-    document.getElementById("#green").addEventListener( 'click' , changeGreen );
-    document.getElementById("#blue").addEventListener( 'click' , changeBlue );
+  document.getElementById("red").addEventListener( 'click' , changeRed );
+  document.getElementById("green").addEventListener( 'click' , changeGreen );
+  document.getElementById("blue").addEventListener( 'click' , changeBlue );
 }
