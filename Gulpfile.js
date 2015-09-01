4fe16a76e7e2de6db16a1f54ae5c032b6fd819f6
Ryan Greene <winchendonsprings@gmail.com>
2015-09-01 00:23:41 -0700

    new revision, still contains commented out old, non browsersync

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,22 +1,54 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 
-//Takes the scss files and converts them to css and will print an error if needed
-gulp.task('styles', function() {
-    gulp.src('scss/**/*.scss')
-        // .pipe(sass().on('error', sass.logError))
+// Normal compile without browsersync
+//
+//                //Takes the scss files and converts them to css and will print an error if needed
+//                gulp.task('styles', function() {
+//                    gulp.src('scss/*.scss')
+//                        // .pipe(sass().on('error', sass.logError))
+//                        .pipe(sass({
+//                            includePaths: require('node-bourbon').includePaths,
+//                            includePaths: require('node-neat').includePaths
+//                        }).on('error', sass.logError))
+//                //	.pipe(sass({
+//                //            includePaths: require('node-neat').includePaths
+//                //        }))
+//                        // .pipe(sass().on('error', sass.logError))
+//                        .pipe(gulp.dest('css/'));
+//                });
+//
+//                //Watch task
+//                gulp.task('default', function() {
+//                    gulp.watch('scss/*.scss',['styles']);
+//                });
+
+
+// With browsersync 
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        // server: "./"
+        proxy: "drupal3.local"
+    });
+
+    gulp.watch("./scss/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("./scss/*.scss")
+        // .pipe(sass())
         .pipe(sass({
             includePaths: require('node-bourbon').includePaths,
             includePaths: require('node-neat').includePaths
         }).on('error', sass.logError))
-//	.pipe(sass({
-//            includePaths: require('node-neat').includePaths
-//        }))
-        // .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('css/'));
+        .pipe(gulp.dest("./css"))
+        .pipe(browserSync.stream());
 });
 
-//Watch task
-gulp.task('default', function() {
-    gulp.watch('scss/**/*.scss',['styles']);
-});
+gulp.task('default', ['serve']);
